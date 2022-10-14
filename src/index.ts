b79c7b2964aa23716b15a075fb01d5cd899490d7
니꼬 <nico@nomadcoders.co>
2022-10-14 15:55:57 +0900

    21.7 Our First Durable Object

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   DB: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+  COUNTER: DurableObjectNamespace;
 }
 
 // @ts-ignore
 import home from "./home.html";
-import { makeBadge } from "./utils";
 
 function handleHome() {
   return new Response(home, {
@@ -27,30 +20,26 @@ function handleNotFound() {
   });
 }
 
-function handleBadRequest() {
-  return new Response(null, {
-    status: 400,
-  });
-}
-
-async function handleVisit(searchParams: URLSearchParams, env: Env) {
-  const page = searchParams.get("page");
-  if (!page) {
-    return handleBadRequest();
+export class CounterObject {
+  counter: number;
+  constructor() {
+    this.counter = 0;
   }
-  const kvPage = await env.DB.get(page);
-  let value = 1;
-  if (!kvPage) {
-    await env.DB.put(page, value + "");
-  } else {
-    value = parseInt(kvPage) + 1;
-    await env.DB.put(page, value + "");
+  async fetch(request: Request) {
+    const { pathname } = new URL(request.url);
+    switch (pathname) {
+      case "/":
+        return new Response(this.counter);
+      case "/+":
+        this.counter++;
+        return new Response(this.counter);
+      case "/-":
+        this.counter--;
+        return new Response(this.counter);
+      default:
+        return handleNotFound();
+    }
   }
-  return new Response(makeBadge(value), {
-    headers: {
-      "Content-Type": "image/svg+xml;charset=utf-8",
-    },
-  });
 }
 
 export default {
@@ -59,14 +48,9 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const { pathname, searchParams } = new URL(request.url);
-    switch (pathname) {
-      case "/":
-        return handleHome();
-      case "/visit":
-        return handleVisit(searchParams, env);
-      default:
-        return handleNotFound();
-    }
+    const id = env.COUNTER.idFromName("counter");
+    const durableObject = env.COUNTER.get(id);
+    const response = await durableObject.fetch(request);
+    return response;
   },
 };
