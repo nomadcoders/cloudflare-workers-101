cfa515796fe057a6f5ac601a05c217e7546307be
니꼬 <nico@nomadcoders.co>
2022-10-14 12:56:46 +0900

    21.4 Visit Counter

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,60 @@ export interface Env {
 // @ts-ignore
 import home from "./home.html";
 
+function handleHome() {
+  return new Response(home, {
+    headers: {
+      "Content-Type": "text/html;chartset=utf-8",
+    },
+  });
+}
+
+function handleNotFound() {
+  return new Response(null, {
+    status: 404,
+  });
+}
+
+function handleBadRequest() {
+  return new Response(null, {
+    status: 400,
+  });
+}
+
+async function handleVisit(searchParams: URLSearchParams, env: Env) {
+  const page = searchParams.get("page");
+  if (!page) {
+    return handleBadRequest();
+  }
+  const kvPage = await env.DB.get(page);
+  let value = 1;
+  if (!kvPage) {
+    await env.DB.put(page, value + "");
+  } else {
+    value = parseInt(kvPage) + 1;
+    await env.DB.put(page, value + "");
+  }
+  return new Response(JSON.stringify({ visits: value }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
 export default {
   async fetch(
     request: Request,
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const url = new URL(request.url);
-    if (url.pathname === "/") {
-      await env.DB.put("hello", "how are you?");
-      return new Response(home, {
-        headers: {
-          "Content-Type": "text/html;chartset=utf-8",
-        },
-      });
+    const { pathname, searchParams } = new URL(request.url);
+    switch (pathname) {
+      case "/":
+        return handleHome();
+      case "/visit":
+        return handleVisit(searchParams, env);
+      default:
+        return handleNotFound();
     }
-    return new Response(null, {
-      status: 404,
-    });
   },
 };
